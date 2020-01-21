b4db08ebfbdaa6ecfb57a6636686fff54cae3d81
jihwan1008 <jihan1008@gist.ac.kr>
2020-01-21 00:58:32 +0000

    tqdm added

---
+++ data/data_prepare.py
@@ -0,0 +1,90 @@
+import sys
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+default_train = './aisegment/clip_img/'
+default_mat = './aisegment/matting/'
+txt_train = './train.txt'
+txt_mat = './mask.txt'
+
+
+tmp_dir = './image/'
+if os.path.isdir(tmp_dir):
+    pass
+else:
+    os.mkdir(tmp_dir)
+
+file_1 = sorted(os.listdir(default_train))
+#print(len(file_1))
+
+tmp_list = []
+tmp_list_ = []
+print('Iterating through reference image directory architecture to get image file names...')
+print('Generating Images folder...')
+for item in file_1:
+    file_2 = sorted(os.listdir(default_train + item))
+    for elem in file_2:
+        file_3 = sorted(os.listdir(default_train + item + '/' + elem))
+        for blob in tqdm(file_3):
+            tmp_list.append(blob)
+            img = cv2.imread(default_train + item + '/' + elem + '/' + blob)
+            cv2.imwrite(tmp_dir + blob, img)
+
+print('Total # of Ref images : {}'.format(len(tmp_list)))
+
+with open(txt_train, 'w') as f:
+    for item in tmp_list:
+        f.write(item)
+        f.write('\n')
+    f.close()
+
+del file_1
+del file_2
+del file_3
+del tmp_dir
+
+print('Iterating through matting image directory architecture to get matting file names...')
+print('Generating Mask images...')
+
+tmp_dir = './mask/'
+
+if os.path.isdir(tmp_dir):
+    pass
+else:
+    os.mkdir(tmp_dir)
+
+file_1 = sorted(os.listdir(default_mat))
+for item in file_1:
+    file_2 = sorted(os.listdir(default_mat + item))
+    for elem in file_2:
+        file_3 = sorted(os.listdir(default_mat + item + '/' + elem))
+        for blob in tqdm(file_3):
+            blob_ = blob[:-4] + '.png'
+            tmp_list_.append(blob_)
+            #Load image with -1 to enforce image to have 4 channels(BRGA)
+            img = cv2.imread(default_mat + item + '/' + elem + '/' + blob, cv2.IMREAD_UNCHANGED)
+            #Alpha channel mask to convert the transparent part to white
+            #https://stackoverflow.com/questions/48816703/opencv-turn-transparent-part-of-png-white
+            alpha = img[:,:,3]
+            _, m = cv2.threshold(alpha, 254, 255, cv2.THRESH_BINARY)
+            color = img[:,:,:3]
+            img = cv2.bitwise_not(cv2.bitwise_not(color, mask=m))
+            #print(img.shape)
+            mask = np.full((img.shape[0], img.shape[1], img.shape[2]), 0, dtype=np.uint8)
+            for i in range(img.shape[0]):
+                for j in range(img.shape[1]):
+                    if not (img[i,j,0] and img[i,j,1] and img[i,j,2]):
+                        mask[i,j,0] = 255
+                        mask[i,j,1] = 255
+                        mask[i,j,2] = 255
+            cv2.imwrite(tmp_dir + blob_, np.invert(mask))
+
+print('Total # of Matting images : {}'.format(len(tmp_list_)))
+
+with open(txt_mat, 'w') as f:
+    for item in tmp_list_:
+        f.write(item)
+        f.write('\n')
+    f.close()
